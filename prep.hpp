f337c845ebb771d9b84325c758f7b8ec1c74be83
Ryan James Zafft <rzafft1@hive1.eecs.uwyo.edu>
2023-02-01 08:21:18 -0700

    prep header file

---
+++ prep.hpp
@@ -0,0 +1,22 @@
+#ifndef PREP_
+#define PREP_
+
+#include "node.h"
+
+                        // we are not using data types (strings, int, etc.)... 
+                        // we are using template instead 
+template <class T>
+
+class Prep
+{
+        private:
+
+                node<T> *head;
+                
+
+        public:
+};
+
+#endif
+
+
