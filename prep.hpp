e7a74ebdf2c1fb334162b59f0b3b538fd30be762
Ryan James Zafft <rzafft1@hive2.eecs.uwyo.edu>
2023-02-01 08:45:40 -0700

    editing prep.hpp

--- prep.hpp
+++ prep.hpp
@@ -5,16 +5,23 @@
 
                         // we are not using data types (strings, int, etc.)... 
                         // we are using template instead 
+			// in main code we can declare the type with a variable
+				// Prep<int> PrepInt;
+				// Prep<string> PrepStr;
+			// compiler will use one of these types at runtime
 template <class T>
 
 class Prep
 {
         private:
 
-                node<T> *head;
-                
+                node<T> *head;   // pointer to data structure
+		int size;	 // count number of objects contained in data structure
 
         public:
+				// constructor, destructor, getSize, and isEmtpy are required
+				// some variation of insert/add, remove/delete, and print
+
 };
 
 #endif
