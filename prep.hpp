ccf332879b13cea1ebbbe97fa15d7a584209e500
Ryan James Zafft <rzafft1@hive2.eecs.uwyo.edu>
2023-02-01 08:51:19 -0700

    hpp edits

--- prep.hpp
+++ prep.hpp
@@ -3,12 +3,12 @@
 
 #include "node.h"
 
-                        // we are not using data types (strings, int, etc.)... 
-                        // we are using template instead 
-			// in main code we can declare the type with a variable
-				// Prep<int> PrepInt;
-				// Prep<string> PrepStr;
-			// compiler will use one of these types at runtime
+                                // we are not using data types (strings, int, etc.)... 
+                                // we are using template instead 
+		        	// in main code we can declare the type with a variable
+			        	// Prep<int> PrepInt;
+				        // Prep<string> PrepStr;
+			        // compiler will use one of these types at runtime
 template <class T>
 
 class Prep
@@ -19,9 +19,14 @@ class Prep
 		int size;	 // count number of objects contained in data structure
 
         public:
-				// constructor, destructor, getSize, and isEmtpy are required
-				// some variation of insert/add, remove/delete, and print
+		     		 // constructor, destructor, getSize, and isEmtpy are required
+				 // some variation of insert/add, remove/delete, and print
 
+		prep()
+		{
+			head = nullptr;
+			size = 0;
+		}
 };
 
 #endif
